6f7e6a84bb49fa3a2edde6314e68dda0976fb24d
mycodestiny <75158166+sunzooprince@users.noreply.github.com>
2020-12-12 19:21:39 -0500

    Final

---
+++ Day 3/Projects/even or odd.py
@@ -0,0 +1,8 @@
+#the purpose of this project states determine whether or not a number is odd or even.
+#while True
+
+number = int(input("What is your number?\n"))
+if number % 2 == 0:
+    print("This number is even")
+else:
+    print("This number is odd")
